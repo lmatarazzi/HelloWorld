f68c0b4bb2d8c8b8f0912711aa24e3ffddd73706
Leandro <l.matarazzi@alice.it>
2018-06-27 17:01:59 +0200

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args) {
 		for (int ii=1;ii<=10;ii++){
-			System.out.println("Hello Second World ... " + ii);
+			System.out.println("Hello Third World ... " + ii);
 		}
 	}
 	
